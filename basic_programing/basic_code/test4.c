21fe6fc0b9246fd5daad8848feb632fe0d4fc881
Sky/S <kor.hui@foxmail.com>
2023-03-16 18:29:31 +0800

    3/16

--- basic_programing/basic_code/test4.c
+++ basic_programing/basic_code/test4.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    
+    int bin,a,b;
+    scanf("%d", &bin);
+    a = bin/16; b = bin%16;
+    if (bin==0)
+    {
+        printf("%d", 0);
+    }
+    else
+    {   
+        printf("%d%d", a,b);
+    }
+
+    return 0;
 }
